f33490edeaf32687233e572ec721528f7d1cc143
leoschmitzqm <a947687206@sina.com>
2021-01-10 18:24:53 +0800

    adding code

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,20 +1,15 @@
 darkBtn.addEventListener('click', () => {
-	// check for event fire
 	console.log('Click event has fired!')
-	// change h1 text
 	message.textContent = 'This is dark mode.'
-	// change body background to black
 	body.style.background = 'black'
-	// change body text to chartreuse
 	message.style.color = 'chartreuse'
-  
   })
   
   const changeToLight = () => {
-	message.textContent = 'This is Light Mode.'
+	message.textContent = 'This is light mode.'
 	body.style.background = 'white'
 	message.style.color = 'black'
   }
   
 
-  lightBtn.addEventListener('click', changeToLight) 
+  lightBtn.addEventListener('click', changeToLight)
